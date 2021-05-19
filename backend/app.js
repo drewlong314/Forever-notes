f4faec749e3a7c6e3d1a88b6cca4b9e228786bac
Drew Long <drewlong314@gmail.com>
2021-05-19 18:11:10 -0500

    initial commit, added boiler plate express/database

---
+++ backend/app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const csurf = require("csurf");
+const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
+const routes = require("./routes");
+
+const { environment } = require("./config");
+const isProduction = environment === "production";
+const app = express();
+app.use(morgan("dev"));
+app.use(cookieParser());
+app.use(express.json());
+
+// Security Middleware
+if (!isProduction) {
+  // enable cors only in development
+  app.use(cors());
+}
+// helmet helps set a variety of headers to better secure your app
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
+
+// Set the _csrf token and create req.csrfToken method
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true,
+    },
+  })
+);
+
+app.use(routes);
+
+module.exports = app;

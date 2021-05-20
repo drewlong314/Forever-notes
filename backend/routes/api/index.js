0a802ad1c755368b904d2abdd2674d7e53e0d315
Drew Long <drewlong314@gmail.com>
2021-05-20 10:48:33 -0500

    user authentication

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,7 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
-  });
+router.post("/test", function (req, res) {
+  res.json({ requestBody: req.body });
+});
 
 module.exports = router;

cd3f380a656427f1a89e3be4c574eeda3861753c
Drew Long <drewlong314@gmail.com>
2021-05-20 11:04:52 -0500

    get session user

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -31,13 +31,18 @@ router.post(
 );
 
 // Log out
-router.delete(
-    '/',
-    (_req, res) => {
-      res.clearCookie('token');
-      return res.json({ message: 'success' });
-    }
-  );
+router.delete("/", (_req, res) => {
+  res.clearCookie("token");
+  return res.json({ message: "success" });
+});
 
-
+// Restore session user
+router.get("/", restoreUser, (req, res) => {
+  const { user } = req;
+  if (user) {
+    return res.json({
+      user: user.toSafeObject(),
+    });
+  } else return res.json({});
+});
 module.exports = router;

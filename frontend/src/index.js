8cbff909215042e43418c2c6737127b1f0ce7bf8
Drew Long <drewlong314@gmail.com>
2021-05-20 12:34:52 -0500

    session redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider as ReduxProvider } from 'react-redux';
-import { restoreCSRF, csrfFetch } from './store/csrf';
-import './index.css';
-import App from './App';
-import configureStore from './store';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider as ReduxProvider } from "react-redux";
+import { restoreCSRF, csrfFetch } from "./store/csrf";
+import "./index.css";
+import App from "./App";
+import configureStore from "./store";
+import * as sessionActions from "./store/session";
 
 const store = configureStore();
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
   restoreCSRF();
 
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 if (process.env.NODE_ENV !== "production") {
@@ -35,5 +36,5 @@ ReactDOM.render(
   <React.StrictMode>
     <Root />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );

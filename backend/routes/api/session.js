1c80fd341ea3013f7bc116e0f960298fe4ff1540
Drew Long <drewlong314@gmail.com>
2021-05-20 10:58:32 -0500

    logout route

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -30,4 +30,14 @@ router.post(
   })
 );
 
+// Log out
+router.delete(
+    '/',
+    (_req, res) => {
+      res.clearCookie('token');
+      return res.json({ message: 'success' });
+    }
+  );
+
+
 module.exports = router;

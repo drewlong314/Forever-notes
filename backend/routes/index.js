0669eff2b08bfc8baf3915a60a0fc3d81726a74c
Drew Long <drewlong314@gmail.com>
2021-05-21 11:53:51 -0500

    heroku set up

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,12 +4,14 @@ const apiRouter = require("./api");
 
 router.use("/api", apiRouter);
 
+// Static routes
+// Serve React build files in production
 if (process.env.NODE_ENV === "production") {
   const path = require("path");
   // Serve the frontend's index.html file at the root route
   router.get("/", (req, res) => {
     res.cookie("XSRF-TOKEN", req.csrfToken());
-    return res.sendFile(
+    res.sendFile(
       path.resolve(__dirname, "../../frontend", "build", "index.html")
     );
   });
@@ -20,16 +22,17 @@ if (process.env.NODE_ENV === "production") {
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   router.get(/^(?!\/?api).*/, (req, res) => {
     res.cookie("XSRF-TOKEN", req.csrfToken());
-    return res.sendFile(
+    res.sendFile(
       path.resolve(__dirname, "../../frontend", "build", "index.html")
     );
   });
 }
 
-if (process.env.NODE_ENV !== "production") { 
+// Add a XSRF-TOKEN cookie in development
+if (process.env.NODE_ENV !== "production") {
   router.get("/api/csrf/restore", (req, res) => {
     res.cookie("XSRF-TOKEN", req.csrfToken());
-    return res.json({});
+    res.status(201).json({});
   });
 }
 

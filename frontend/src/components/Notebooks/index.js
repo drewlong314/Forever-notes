880f661bdfbb97c7778606ca5c41e2a1ece55de8
Drew Long <drewlong314@gmail.com>
2021-05-24 17:16:22 -0500

    css improvment notebook

--- frontend/src/components/Notebooks/index.js
+++ frontend/src/components/Notebooks/index.js
@@ -7,12 +7,15 @@ import {
   deleteNotebook,
   createNotebook,
 } from "../../store/notebooks";
+import * as sessionActions from "../../store/session";
+import Navigation from "../Navigation";
 import styles from "./Notebooks.module.css";
 
 const Notebooks = () => {
   const dispatch = useDispatch();
   const notebooks = useSelector((state) => Object.values(state.notebooks));
   const sessionUser = useSelector((state) => state.session.user);
+  const [isLoaded, setIsLoaded] = useState(false);
   const [selected, setSelected] = useState();
   const [selectedName, setSelectedName] = useState();
   const [editArea, setEditArea] = useState(false);
@@ -21,6 +24,10 @@ const Notebooks = () => {
   const [changed, setChanged] = useState(false);
   const history = useHistory();
   console.log(sessionUser, notebooks);
+
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
+  }, [dispatch]);
 
   useEffect(() => {
     if (sessionUser) {
@@ -39,38 +46,73 @@ const Notebooks = () => {
 
   return (
     <div className={styles.container}>
-      <h1>This is Notebooks</h1>
-      <ul>
-        {notebooks.map((notebook) => {
-          return (
-            <li
-              key={notebook.id}
-              onClick={() => {
-                setSelected(notebook);
-                setSelectedName(notebook.name);
-              }}
-            >
-              Notebook Name: {notebook.name}
-            </li>
-          );
-        })}
-      </ul>
-      <button onClick={() => setEditArea(true)}>EDIT</button>
-      <button onClick={() => dispatch(deleteNotebook(selected.id))}>
-        DELETE
-      </button>
-      {editArea ? (
-        <textarea
-          value={selectedName}
-          onChange={(e) => {
-            console.log(selected.id);
-            setChanged(true);
-            setSelectedName(e.target.value);
-          }}
-        ></textarea>
-      ) : null}
-      <button onClick={() => setCreateArea(true)}>CREATE</button>
-      {createArea ? (
+      <Navigation isLoaded={isLoaded} />
+      <div className={styles.notebookSelect}>
+        <div className={styles.notebookHeader}>
+          <p>Notebooks</p>
+          <input
+            className={styles.notebookSearch}
+            type="text"
+            placeholder="Find Notebooks..."
+          ></input>
+        </div>
+        <div className={styles.notebookInfo}>
+          <p>{`${notebooks.length} notebooks`}</p>
+          <button
+            className={styles.notebookCreate}
+            onClick={() => setCreateArea(true)}
+          >
+            New Notebook
+          </button>
+        </div>
+        <div className={styles.notebookStats}>
+          <p className={styles.notebookTitle}>TITLE</p>
+          <p className={styles.notebookCreated}>CREATED BY</p>
+          <p className={styles.notebookUpdated}>UPDATED</p>
+          <p className={styles.notebookAction}>ACTIONS</p>
+        </div>
+        <ul className={styles.notebookList}>
+          {notebooks.map((notebook) => {
+            return (
+              <div className={styles.notebookChoice}>
+                <li
+                  className={styles.notebookTitle}
+                  key={notebook.id}
+                  onClick={() => {
+                    setSelected(notebook);
+                    setSelectedName(notebook.name);
+                  }}
+                >
+                  <button>E</button> {notebook.name}
+                </li>
+                <p className={styles.notebookCreated}>{sessionUser.username}</p>
+                <p className={styles.notebookUpdated}>
+                  {notebook.updatedAt.split("T")[0]}
+                </p>
+                <div className={styles.notebookDot}>
+                  <button className={styles.notebookAction}> ...</button>
+                </div>
+              </div>
+            );
+          })}
+        </ul>
+        <button onClick={() => setEditArea(true)}>EDIT</button>
+        <button onClick={() => dispatch(deleteNotebook(selected.id))}>
+          DELETE
+        </button>
+
+        {editArea ? (
+          <textarea
+            value={selectedName}
+            onChange={(e) => {
+              console.log(selected.id);
+              setChanged(true);
+              setSelectedName(e.target.value);
+            }}
+          ></textarea>
+        ) : null}
+      </div>
+      {createArea ? ( // Make this a modal
         <form
           onSubmit={(e) => {
             e.preventDefault();
